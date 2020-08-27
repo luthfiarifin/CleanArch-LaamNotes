b987c9e20c029549ad95bca0f4ba3ce28bbff7eb
Luthfi Arifin <luthfiarifin0222@gmail.com>
2020-08-27 22:53:06 +0700

    Move MainActivity.kt to presentation package in app module

--- app/src/main/java/com/laam/laamnotes/presentation/MainActivity.kt
+++ app/src/main/java/com/laam/laamnotes/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.laam.laamnotes
+package com.laam.laamnotes.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.laam.laamnotes.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
