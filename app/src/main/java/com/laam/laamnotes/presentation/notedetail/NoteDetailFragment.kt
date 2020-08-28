465ed7e46d0665dffc4925ac497480cbbffd68b7
Luthfi Arifin <luthfiarifin0222@gmail.com>
2020-08-28 17:05:41 +0700

    Configure NoteDetailFragment.kt save menu

--- app/src/main/java/com/laam/laamnotes/presentation/notedetail/NoteDetailFragment.kt
+++ app/src/main/java/com/laam/laamnotes/presentation/notedetail/NoteDetailFragment.kt
@@ -1,9 +1,13 @@
 package com.laam.laamnotes.presentation.notedetail
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import com.laam.laamnotes.R
 import com.laam.laamnotes.databinding.FragmentNoteDetailBinding
+import com.laam.laamnotes.databinding.FragmentNoteListBinding
 import com.laam.laamnotes.presentation.common.BaseFragment
 
 class NoteDetailFragment : BaseFragment<FragmentNoteDetailBinding, NoteDetailViewModel>(),
@@ -15,5 +19,21 @@ class NoteDetailFragment : BaseFragment<FragmentNoteDetailBinding, NoteDetailVie
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_save, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item_save -> {
+                true
+            }
+            else ->
+                return super.onOptionsItemSelected(item)
+        }
     }
 }
