b5d21916e035772090aac7620c2c96b2c42f9dae
Luthfi Arifin <luthfiarifin0222@gmail.com>
2020-10-06 06:21:50 +0700

    Add back navigation icon

--- app/src/main/java/com/laam/laamnotes/presentation/main/MainActivity.kt
+++ app/src/main/java/com/laam/laamnotes/presentation/main/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.laam.laamnotes.presentation.main
 
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.laam.laamnotes.R
 import com.laam.laamnotes.databinding.ActivityMainBinding
 import com.laam.laamnotes.presentation.common.BaseActivity
@@ -10,9 +15,23 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(), MainNav
 
     override fun getLayoutId(): Int = R.layout.activity_main
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         viewModel.setNavigator(this)
+        setUpNavigation()
     }
+
+    private fun setUpNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+
+        navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean =
+        NavigationUI.navigateUp(navController, null)
 }
