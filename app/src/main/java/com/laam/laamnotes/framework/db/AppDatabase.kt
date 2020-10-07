d1c90ab68cb122616701e5dc8ea77fe3966d66fc
Luthfi Arifin <luthfiarifin0222@gmail.com>
2020-10-08 06:34:19 +0700

    Add NoteEntity in AppDatabase.kt

--- app/src/main/java/com/laam/laamnotes/framework/db/AppDatabase.kt
+++ app/src/main/java/com/laam/laamnotes/framework/db/AppDatabase.kt
@@ -4,10 +4,12 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.laam.laamnotes.framework.db.dao.ImageDao
 import com.laam.laamnotes.framework.db.dao.NoteDao
+import com.laam.laamnotes.framework.db.entity.ImageEntity
 import com.laam.laamnotes.framework.db.entity.NoteEntity
 
-@Database(entities = [NoteEntity::class], version = 1)
+@Database(entities = [NoteEntity::class, ImageEntity::class], version = 2)
 abstract class AppDatabase : RoomDatabase() {
 
     companion object {
@@ -20,4 +22,5 @@ abstract class AppDatabase : RoomDatabase() {
     }
 
     abstract fun noteDao(): NoteDao
+    abstract fun imageDao(): ImageDao
 }
