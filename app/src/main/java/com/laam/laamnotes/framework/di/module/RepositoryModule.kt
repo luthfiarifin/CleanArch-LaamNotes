5c51e6c76502865d21114d2740e5ea0cd7d9884f
Luthfi Arifin <luthfiarifin0222@gmail.com>
2020-10-12 06:19:56 +0700

    Add provide `provideImageRepository()` in RepositoryModule.kt

--- app/src/main/java/com/laam/laamnotes/framework/di/module/RepositoryModule.kt
+++ app/src/main/java/com/laam/laamnotes/framework/di/module/RepositoryModule.kt
@@ -1,6 +1,8 @@
 package com.laam.laamnotes.framework.di.module
 
+import com.laam.core.repository.image.ImageRepository
 import com.laam.core.repository.note.NoteRepository
+import com.laam.laamnotes.framework.datasource.ImageDataSource
 import com.laam.laamnotes.framework.datasource.NoteDataSource
 import dagger.Module
 import dagger.Provides
@@ -12,4 +14,9 @@ object RepositoryModule {
     @JvmStatic
     fun provideNoteRepository(dataSource: NoteDataSource) =
         NoteRepository(dataSource)
+
+    @Provides
+    @JvmStatic
+    fun provideImageRepository(dataSource: ImageDataSource) =
+        ImageRepository(dataSource)
 }
